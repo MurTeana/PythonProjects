b24ae3d0f448a007a569e13b75d4beefa3b541ff
MurTeana <sm.teana@gmail.com>
2023-11-19 19:04:21 +0300

    json edit

--- CSVtoJSON/main.py
+++ CSVtoJSON/main.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import json
-from datetime import datetime
 
 # Чтение CSV файла
 csv_file_path = 'D:\Coding\PYTHON\PythonProjects\CSVtoJSON\data\csv\Canada_16_11_23_sample.csv'
@@ -11,37 +10,47 @@ print(df.columns)
 json_data = []
 for index, row in df.iterrows():
     parcel = {
-         "parcelType": "103",
-         "parcels": [
-             {
-                "additionalServices": [
-                    {"code": "DOB", "amount": 0.0},
-                    {"code": "V", "amount": row.get("item-price")},
-                    {"code": "ODK", "amount": row.get("item-tax")}
-                ],
-                "parcelNumber": "",
-                "weight": "",
-                "volume": 0.0,
-                "cn23": None,
-                "international": None,
-                "parcelDate": ""
-             }
-         ],
+        "parcelType": 103,
+        "parcels": [{
+            "parcelNumber": row.get("order-item-id"),
+            "weight": None,
+            "volume": None,
+            "additionalServices": [
+                {"code": "ODK", "amount": None},
+                {"code": "V", "amount": row.get("item-price")},
+                {"code": "PR", "amount": None}],
+            "cn23": None,
+            "international": {
+                "nonDeliveryInstruction": 1,  # (1 = return to sender, 2 = sender waives its claim to the parcel)
+                "categoryType": 5,  # 5 = sale of goods
+                "categoryExplanation": None,
+                "importer": None,
+                "importerPhone": None,
+                "importerEmail": None,
+                "comments": None,
+                "license": None,
+                "certificate": None,
+                "invoice": None,
+                "codForeign": None,
+                "codForeignCurrency": row.get("currency")
+            },
+            "parcelContent": 1  # for goods (1) or documents (0).
+        }],
         "commonServices": None,
         "addressee": {
-            "addresseeName1": row.get("buyer-name", ""),
-            "addresseeName2": "",
-            "addresseeAddress": row.get("ship-address-1", ""),
-            "addresseePostId": row.get("ship-postal-code", ""),
-            "addresseePost": row.get("ship-city", ""),
-            "addresseeCountryId": row.get("ship-country", ""),
-            "addresseePhone": row.get("ship-phone-number", ""),
-            "addresseeEmail": row.get("buyer-email", "")
+            "addresseeName1": row.get("recipient-name", ""),
+            "addresseeName2": row.get("buyer-name", ""),
+            "addresseeAddress": row.get("ship-address-1"),
+            "addresseePostId": row.get("ship-postal-code"),
+            "addresseePost": row.get("ship-city"),
+            "addresseeCountryId": "124",  # for Canada
+            "addresseePhone": row.get("ship-phone-number"),
+            "addresseeEmail": row.get("buyer-email")
         },
         "upn": None,
-        "comment1": row["purchase-date"],
-        "comment2": row["delivery-Instructions"],
-        "recordNumber": row["order-item-id"]
+        "comment1": row.get("ship-state"),
+        "comment2": row.get("sku"),
+        "recordNumber": row.get("order-id")
     }
 
     json_data.append(parcel)
